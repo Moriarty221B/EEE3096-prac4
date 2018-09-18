b8e8dd3be17d5772813ef152d9ecae4eb97c9445
tnyazenga <43371794+tnyazenga@users.noreply.github.com>
2018-09-18 20:25:15 +0200

    Update prac4.py
    added callback functions for display and stop buttons and event detection

--- prac4.py
+++ prac4.py
@@ -21,3 +21,28 @@ GPIO.setup(frequency, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(stop, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(display, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+
+#interrupt to stop recording data
+def stopcallback(channel):
+    global monitor
+    global log_count
+    
+    if monitor == 1:
+        monitor = 0
+    elif monitor == 0:
+        monitor = 1
+        log_count = 0
+        
+#display button callback function
+def displaycallback(channel):
+    global monitor
+    global log
+    if monitor == 0:
+        print("Time     Timer    pot   Temp  Light")
+        print(log)
+
+# callback functions will be called
+GPIO.add_event_detect(reset, GPIO.RISING, callback=resetcallback,bouncetime=200)
+GPIO.add_event_detect(frequency, GPIO.RISING, callback=freqcallback,bouncetime=200)
+GPIO.add_event_detect(stop, GPIO.RISING, callback=stopcallback,bouncetime=200)
+GPIO.add_event_detect(display, GPIO.RISING, callback=displaycallback,bouncetime=200)
